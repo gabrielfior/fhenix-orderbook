6ce0ad22cb53c2b3896a83d41716ad24a1154932
Gabriel Fior <fior.gbm@gmail.com>
2024-03-02 08:30:38 -0700

    chore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,6 +15,7 @@ dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
 
 //const TESTNET_RPC_URL = "https://api.testnet.fhenix.zone:7747";
 const TESTNET_RPC_URL = "http://localhost:42069";
+const dev_account = process.env.KEY_DEV;
 
 
 const config: HardhatUserConfig = {
@@ -25,12 +26,12 @@ const config: HardhatUserConfig = {
     localfhenix: {
       chainId: 412346,
       url: "http://localhost:42069",
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"] // hardhat
     },
     testnet: {
       chainId: 42069,
       url: "https://api.testnet.fhenix.zone:7747",
-      accounts: ["ece6dc6f5020f6fdb05bdae8fc012a7721336bde780ddd423aa41e3a8adb0c89"]
+      accounts: [dev_account!]
     },//testnetConfig,
   },
   typechain: {
